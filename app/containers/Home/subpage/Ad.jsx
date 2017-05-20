144abf016cb571bf6233d18fe18c0b8d1e99326d
卢德政 <ludzh@naihaideMacBook-Pro.local>
2017-05-20 13:05:18 +0800

    更新猜你喜欢

--- app/containers/Home/subpage/Ad.jsx
+++ app/containers/Home/subpage/Ad.jsx
@@ -1,47 +1,74 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import HomeAd from '../../../components/HomeAd/index'
-import { getAdData } from '../../../fetch/home/home'
 
+import { getAdData } from '../../../fetch/home/home.js'
+import HomeAd from '../../../components/HomeAd/index.jsx'
+ 
 class Ad extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
-            data: []
-        }
+            // data: [] 
+            data: [
+                {
+                    title: '暑假5折',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191639092-2000037796.png',
+                    link: 'https://github.com/dezhenglu/'
+                },
+                {
+                    title: '特价出国',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191648124-298129318.png',
+                    link: 'https://github.com/dezhenglu/'
+                },
+                {
+                    title: '亮亮车',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191653983-1962772127.png',
+                    link: 'https://github.com/dezhenglu/'
+                },
+                {
+                    title: '学钢琴',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191700420-1584459466.png',
+                    link: 'https://github.com/dezhenglu/'
+                },
+                {
+                    title: '电影',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191706733-367929553.png',
+                    link: 'https://github.com/dezhenglu/'
+                },
+                {
+                    title: '旅游热线',
+                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016191713186-495002222.png',
+                    link: 'https://github.com/dezhenglu/'
+                }
+            ]
+        };
     }
     render() {
         return (
             <div>
-            {
-                this.state.data.length
-                ? <HomeAd data={this.state.data}/>
-                : <div>{/* 加载中... */}</div>
-            }
+                {
+                    this.state.data.length
+                    ? <HomeAd data={ this.state.data }/>
+                    : <div>加载中...</div>
+                }
             </div>
         )
     }
     componentDidMount() {
-        // 获取广告数据
-        const result = getAdData()
-        result.then(res => {
-            return res.json()
-        }).then(json => {
-            // 处理获取的数据
-            const data = json
-            if (data.length) {
-                this.setState({
-                    data: data
-                })
-            }
-        }).catch(ex => {
-            // 发生错误
-            if (__DEV__) {
-                console.error('首页广告模块获取数据报错, ', ex.message)
-            }
-        })
+        // const result = getAdData();
+        // result.then((res) => {
+        //     return res.json()
+        // }).then((json) => {
+        //     const data = json
+        //     if(data.length) {
+        //         this.setState({
+        //             data: data 
+        //         });
+        //     }
+        // })
     }
 }
+
 
 export default Ad
