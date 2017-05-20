b9abf755402579ab451eb91f3fc0c77ac09cadae
卢德政 <ludzh@naihaideMacBook-Pro.local>
2017-05-20 15:53:58 +0800

    mock data update

--- app/containers/Home/subpage/List.jsx
+++ app/containers/Home/subpage/List.jsx
@@ -9,49 +9,49 @@ class List extends React.Component {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
-            // data: [],
-            data: [
-                {
-                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201638030-473660627.png',
-                    title: '汉堡大大',
-                    subTitle: '叫我汉堡大大，还你多彩口味',
-                    price: '28',
-                    distance: '120m',
-                    mumber: '389'
-                },
-                {
-                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201645858-1342445625.png',
-                    title: '北京开源饭店',
-                    subTitle: '[望京]自助晚餐',
-                    price: '98',
-                    distance: '140m',
-                    mumber: '689'
-                },
-                {
-                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201652952-1050532278.png',
-                    title: '服装定制',
-                    subTitle: '原价xx元，现价xx元，可修改一次',
-                    price: '1980',
-                    distance: '160',
-                    mumber: '106'
-                },
-                {
-                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201700186-1351787273.png',
-                    title: '婚纱摄影',
-                    subTitle: '免费试穿，拍照留念',
-                    price: '2899',
-                    distance: '160',
-                    mumber: '58'
-                },
-                {
-                    img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201708124-1116595594.png',
-                    title: '麻辣串串烧',
-                    subTitle: '双人免费套餐等你抢购',
-                    price: '0',
-                    distance: '160',
-                    mumber: '1426'
-                }
-            ],
+            data: [],
+            // data: [
+            //     {
+            //         img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201638030-473660627.png',
+            //         title: '汉堡大大',
+            //         subTitle: '叫我汉堡大大，还你多彩口味',
+            //         price: '28',
+            //         distance: '120m',
+            //         mumber: '389'
+            //     },
+            //     {
+            //         img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201645858-1342445625.png',
+            //         title: '北京开源饭店',
+            //         subTitle: '[望京]自助晚餐',
+            //         price: '98',
+            //         distance: '140m',
+            //         mumber: '689'
+            //     },
+            //     {
+            //         img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201652952-1050532278.png',
+            //         title: '服装定制',
+            //         subTitle: '原价xx元，现价xx元，可修改一次',
+            //         price: '1980',
+            //         distance: '160',
+            //         mumber: '106'
+            //     },
+            //     {
+            //         img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201700186-1351787273.png',
+            //         title: '婚纱摄影',
+            //         subTitle: '免费试穿，拍照留念',
+            //         price: '2899',
+            //         distance: '160',
+            //         mumber: '58'
+            //     },
+            //     {
+            //         img: 'http://images2015.cnblogs.com/blog/138012/201610/138012-20161016201708124-1116595594.png',
+            //         title: '麻辣串串烧',
+            //         subTitle: '双人免费套餐等你抢购',
+            //         price: '0',
+            //         distance: '160',
+            //         mumber: '1426'
+            //     }
+            // ],
             hasMore: false
         };
     }
@@ -70,7 +70,7 @@ class List extends React.Component {
     }
     componentDidMount() {
         // 获取首页数据
-        // this.loadFirstPageData()
+        this.loadFirstPageData()
     }
     // 获取首屏数据
     loadFirstPageData() {
